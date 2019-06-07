a38acddeb9c3106a7d5baee561413b5c7d9a61ce
Toshi <careyeg@gmail.com>
2019-06-07 16:54:52 -0700

    stuff fast

--- server.js
+++ server.js
@@ -2,25 +2,32 @@
 
 require('dotenv').config();
 const express = require('express');
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.listen(3000, () => console.log('I am alive'));
 
-app.use(express.static('./public'));
-
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
+app.get('/ping', (request, response) => {
+  response.send('pong');
 });
 
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained'
-  }
-  response.status(200).json(airplanes);
-});
+// const PORT = process.env.PORT || 3000;
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+// app.use(express.static('./public'));
 
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+// app.get('/hello', (request, response) => {
+//   response.status(200).send('Hello');
+// });
+
+// app.get('/data', (request, response) => {
+//   let airplanes = {
+//     departure: Date.now(),
+//     canFly: true,
+//     pilot: 'Well Trained'
+//   }
+//   response.status(200).json(airplanes);
+// });
+
+// app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+
+// app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
