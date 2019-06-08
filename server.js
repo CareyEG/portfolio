e3b8fb010772c353aba51df821177efe5e86512a
Toshi <careyeg@gmail.com>
2019-06-07 17:09:17 -0700

    fixszszsxxszzss

--- server.js
+++ server.js
@@ -5,29 +5,24 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('I am alive'));
 
-app.get('/ping', (request, response) => {
-  response.send('pong');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
 });
 
-// const PORT = process.env.PORT || 3000;
+app.get('/data', (request, response) => {
+  let airplanes = {
+    departure: Date.now(),
+    canFly: true,
+    pilot: 'Well Trained'
+  }
+  response.status(200).json(airplanes);
+});
 
-// app.use(express.static('./public'));
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
 
-// app.get('/hello', (request, response) => {
-//   response.status(200).send('Hello');
-// });
-
-// app.get('/data', (request, response) => {
-//   let airplanes = {
-//     departure: Date.now(),
-//     canFly: true,
-//     pilot: 'Well Trained'
-//   }
-//   response.status(200).json(airplanes);
-// });
-
-// app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
-
-// app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
